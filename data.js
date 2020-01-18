04f013126f26ab55eabce5d9beebe84fe13f11dd
Walter Hoerman <whoerman@gmail.com>
2020-01-18 09:03:07 -0500

    putting array questions onto page

---
+++ data.js
@@ -0,0 +1,17 @@
+let dataA = [
+    questionX : "What is my new grandaughter's name?",
+    answerA : "Sally",
+    answerB : "Jill",
+    answerC : "Clara",
+    answerD : "Maeve",
+    correctAnswer: "C",
+]
+
+let dataB = [
+    questionX : "What is my first grandaughter's name?",
+    answerA : "Sally",
+    answerB : "Jill",
+    answerC : "Clara",
+    answerD : "Maeve",
+    correctAnswer: "D",
+]
--- mainjs.js
+++ mainjs.js
@@ -1,8 +1,39 @@
-// getting my targets on the html
-let question = document.querySelector("quest");
-let optionA = document.querySelector("optA");
-let optionB = document.querySelector("optB");
-let optionC= document.querySelector("optC");
-let optionD = document.querySelector("optD");
-let answer1 = document.querySelector("RorWr")
+ let dataA = {
+     questionX : "What is my new grandaughter's name?",
+     answerA : "Sally",
+     answerB : "Jill",
+     answerC : "Clara",
+     answerD : "Maeve",
+     correctAnswer: "C",
+}
 
+let dataB = {
+    questionX : "What is my first grandaughter's name?",
+    answerA : "Sally",
+    answerB : "Jill",
+    answerC : "Clara",
+    answerD : "Maeve",
+    correctAnswer: "D",
+}
+
+let dataArray = [dataA, dataB]
+
+// getting my targets on the html
+let question = document.querySelector("#quest");
+let optionA = document.querySelector("#optA");
+let optionB = document.querySelector("#optB");
+let optionC= document.querySelector("#optC");
+let optionD = document.querySelector("#optD");
+let answer1 = document.querySelector("#RorWr")
+
+
+//getting the dta for each question
+let currentQuestion = dataA
+console.log(currentQuestion)
+
+// putting the question on the page
+question.textContent = currentQuestion.questionX;
+optionA.textContent = "A. " + currentQuestion.answerA;
+optionB.textContent = "B. " + currentQuestion.answerB;
+optionC.textContent = "C. " + currentQuestion.answerC;
+optionD.textContent = "D. " + currentQuestion.answerD;
