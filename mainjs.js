b6e9af948ee10d0c639d997b0e12a52a4087748b
Walter Hoerman <whoerman@gmail.com>
2020-01-18 08:04:58 -0500

    main file structure setup

---
+++ mainjs.js
@@ -0,0 +1,8 @@
+// getting my targets on the html
+let question = document.querySelector("quest");
+let optionA = document.querySelector("optA");
+let optionB = document.querySelector("optB");
+let optionC= document.querySelector("optC");
+let optionD = document.querySelector("optD");
+let answer1 = document.querySelector("RorWr")
+
