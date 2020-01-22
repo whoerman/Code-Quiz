ae0cdcce12e28fd534d249e684a33db6be1aabb9
Walter Hoerman <whoerman@gmail.com>
2020-01-22 13:33:11 -0500

    fixed the end of data problems

--- data.js
+++ data.js
@@ -1,26 +1,98 @@
-let dataA = [
-    "What is my new grandaughter's name?",
-    "Sally",
-    "Jill",
-    "Clara",
-    "Maeve",
-    "Clara"
-]
+let dataArray = [
+    [
 
-let dataB = [
-    "What is my first grandaughter's name?",
-    "Sally",
-    "Jill",
-    "Clara",
-    "Maeve",
-    "Maeve"
-]
+    ],
+    
+    ["What is my new grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Clara"
+    ],
 
-let dataC = [
-    "What is my name?",
-    "Sally",
-    "Walter",
-    "Clara",
-    "Maeve",
-    "Walter"
+    ["What is my first grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Maeve"
+    ],
+
+    ["What is my name?",
+        "Sally",
+        "Walter",
+        "Clara",
+        "Maeve",
+        "Walter"
+    ],
+    ["What is my new grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Clara"
+    ],
+
+    ["What is my first grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Maeve"
+    ],
+
+    ["What is my name?",
+        "Sally",
+        "Walter",
+        "Clara",
+        "Maeve",
+        "Walter"
+    ],
+    ["What is my new grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Clara"
+    ],
+
+    ["What is my first grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Maeve"
+    ],
+
+    ["What is my name?",
+        "Sally",
+        "Walter",
+        "Clara",
+        "Maeve",
+        "Walter"
+    ],
+    ["What is my new grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Clara"
+    ],
+
+    ["What is my first grandaughter's name?",
+        "Sally",
+        "Jill",
+        "Clara",
+        "Maeve",
+        "Maeve"
+    ],
+
+    ["What is my name?",
+        "Sally",
+        "Walter",
+        "Clara",
+        "Maeve",
+        "Walter"
+    ]
 ]
--- mainjs.js
+++ mainjs.js
@@ -1,60 +1,85 @@
-let dataArray = [dataA, dataB, dataC]
+//intializing variables
+let x = 0;
+let wrongA = 0;
+let rightA = 0;
+totalAnswers = 0;
 
-let x = 1;
-
-//putting the question on the page
-
-function loadQuestion() {
-$("#question").empty().text(dataArray [x] [0]);
-$("#optionA").empty().text("A. " + dataArray [x] [1]).animate({left:"100px"},2000);
-$("#optionB").empty().text("B. " + dataArray [x] [2]);
-$("#optionC").empty().text("C. " + dataArray [x] [3]);
-$("#optionD").empty().text("D. " + dataArray [x] [4]);
-};
-
-function URCorrect() {
-    console.log("correct");
-};
-
-function URWrong() {
-    console.log("wrong");
-};
-
+//intial load question
 loadQuestion();
 
+//putting the new question on the page and loading data for answers
+function loadQuestion() {
+    x = x + 1;
+    $("#question").empty().text((x + 1) + ": " + dataArray[x][0]);
+    $("#optionA").empty().text("A. " + dataArray[x][1]);
+    $("#optionB").empty().text("B. " + dataArray[x][2]);
+    $("#optionC").empty().text("C. " + dataArray[x][3]);
+    $("#optionD").empty().text("D. " + dataArray[x][4]);
+};
+
 //listen for clicking
-$(document).ready(function() {
-    $("#optionA").click(function() {
+$(document).ready(function () {
+    $("#optionA").click(function () {
         if (dataArray[x][1] === dataArray[x][5]) {
             URCorrect();
+            return;
         };
-        if (dataArray[x][1] !== dataArray[x][5]) {
-            URWrong();
-        };
+        URWrong();
     });
-    $("#optionB").click(function() {
+    $("#optionB").click(function () {
         if (dataArray[x][2] === dataArray[x][5]) {
             URCorrect();
+            return;
         };
-        if (dataArray[x][2] !== dataArray[x][5]) {
-            URWrong();
-        };
+        URWrong();
     });
-    $("#optionC").click(function() {
+    $("#optionC").click(function () {
+
         if (dataArray[x][3] === dataArray[x][5]) {
             URCorrect();
+            return;
         };
-        if (dataArray[x][3] !== dataArray[x][5]) {
-            URWrong();
-        };
+        URWrong();
     });
-    $("#optionD").click(function() {
+    $("#optionD").click(function () {
+
         if (dataArray[x][4] === dataArray[x][5]) {
             URCorrect();
+            return;
         };
-        if (dataArray[x][4] !== dataArray[x][5]) {
-            URWrong();
-        };
+        URWrong();
     });
-                
 });
+
+//If the question is answered correctly
+function URCorrect() {
+    $("#congrats").text("Correct!");
+    rightA = rightA + 1;
+    checkStatus();
+    loadQuestion();
+};
+
+//if the question is answered incorretly
+function URWrong() {
+    $("#congrats").text("Wrong!");
+    wrongA = wrongA + 1;
+    checkStatus();
+    loadQuestion();
+};
+
+//check the status of the game after an answer
+function checkStatus() {
+    console.log("Right Answer " + rightA + " Wrong Answer " + wrongA);
+    totalAnswers = rightA + wrongA;
+    console.log(totalAnswers + " vs " + (dataArray.length - 1));
+    if (totalAnswers < dataArray.length - 1) {
+        return;
+    }
+    endGame();
+};
+
+//end Game process
+function endGame() {
+    console.log("game Over");
+    alert("game over");
+};
