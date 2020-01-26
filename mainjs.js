48e0842d0fd9121e617189a159e9681965f72cb5
Walter Hoerman <whoerman@gmail.com>
2020-01-26 08:44:27 -0500

    Trying to end the quiz timer

--- mainjs.js
+++ mainjs.js
@@ -5,14 +5,21 @@ let rightA = 0;
 totalAnswers = 0;
 let timeLeft = 90;
 let pageArray = localStorage.getItem("storedArray");
+let timerStarted = false;
 
 //start coundown timer
+function startTimer() {
+    timerStarted = true;
 countdownID = setInterval(function() {
     timeLeft--;
     console.log(timeLeft);
     $countdownTimer.empty().text(JSON.stringify(timeLeft) + " seconds");
+    if (timeLeft < 0) {
+        clearInterval(countdownID);
+        endGame();
+    }
 }, 1000);
-
+};
 
 
 //finding the targets for scoring and questions variable
@@ -32,12 +39,12 @@ let $rightAnswerLog = $("#rightAnswerLog");
 let $wrongAnswerLog = $("#wrongAnswerLog");
 
 //intial load question
+$countdownTimer.empty().text("(will start on first click)");
 loadQuestion();
 
 //putting the new question on the page and loading data for answers
 function loadQuestion() {
     x = x + 1;
-    $countdownTimer.empty().text("Calculating");
     $rightAnswerLog.empty().text("Right Answers: " + JSON.stringify(rightA));
     $wrongAnswerLog.empty().text("Wrong Answers: " + JSON.stringify(wrongA));
     $questionText.empty().text((x) + ": " + dataArray[x][0]);
@@ -49,26 +56,23 @@ function loadQuestion() {
 
 //If the question is answered correctly
 function URCorrect() {
-    $congratsText.text("Correct!");
+    $congratsText.text("Correct! 2 second bonus!");
     rightA = rightA + 1;
+    timeLeft = timeLeft +2;
     checkStatus();
     loadQuestion();
 };
 
 //if the question is answered incorretly
 function URWrong() {
-    $congratsText.text("Wrong!");
+    $congratsText.text("Wrong! 5 second penalty!");
     wrongA = wrongA + 1;
-    penalty();
+    timeLeft = timeLeft-5;
     $wrongbtnText.empty().text(wrongA);
     checkStatus();
     loadQuestion();
 };
 
-//subtracting time for wrong answer
-function penalty() {
-    timeLeft = timeLeft - 5;
-}
 
 //check the status of the game after an answer
 function checkStatus() {
@@ -90,6 +94,10 @@ function endGame() {
 //listen for clicking
 $(document).ready(function () {
     $("#optionA").on("click", function () {
+        if (timerStarted === false) {
+            startTimer();
+            console.log(timerStarted);
+        };
         if (dataArray[x][1] === dataArray[x][5]) {
             URCorrect();
             return;
@@ -97,6 +105,9 @@ $(document).ready(function () {
         URWrong();
     });
     $("#optionB").on("click", function () {
+        if (timerStarted ===false) {
+            startTimer();
+        };
         if (dataArray[x][2] === dataArray[x][5]) {
             URCorrect();
             return;
@@ -104,7 +115,10 @@ $(document).ready(function () {
         URWrong();
     });
     $("#optionC").on("click", function () {
-
+        if (timerStarted ===false) {
+            startTimer();
+        };
+        
         if (dataArray[x][3] === dataArray[x][5]) {
             URCorrect();
             return;
@@ -112,7 +126,9 @@ $(document).ready(function () {
         URWrong();
     });
     $("#optionD").on("click", function () {
-
+        if (timerStarted ===false) {
+            startTimer();
+        };
         if (dataArray[x][4] === dataArray[x][5]) {
             URCorrect();
             return;
@@ -120,22 +136,6 @@ $(document).ready(function () {
         URWrong();
     });
 });
-
-//If the question is answered correctly
-function URCorrect() {
-    $("#congrats").text("Correct!");
-    rightA = rightA + 1;
-    checkStatus();
-    loadQuestion();
-};
-
-//if the question is answered incorretly
-function URWrong() {
-    $("#congrats").text("Wrong!");
-    wrongA = wrongA + 1;
-    checkStatus();
-    loadQuestion();
-};
 
 //check the status of the game after an answer
 function checkStatus() {
@@ -150,8 +150,12 @@ function checkStatus() {
 
 //end Game process
 function endGame() {
+    clearInterval(timeLeft);
+    $countdownTimer.empty().text("Game Over!");
     console.log("game Over");
     alert("game over");
+    window.open("./results.html");
+    
 };
 
 
